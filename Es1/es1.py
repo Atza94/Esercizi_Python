dee712f2e0099366cdd010a0c317f591ad020fb5
Atza94 <andrea.atza@gmail.com>
2023-03-28 17:59:01 +0200

    esercizio1

---
+++ Es1/es1.py
@@ -0,0 +1,25 @@
+import streamlit as st
+
+def main():
+    st.title("Primo esercizio")
+    st.header("Idoneità alla guida")
+
+    age=st.number_input(label="Inserisci la tua età", max_value=100, min_value=0 )
+
+    patente = st.radio(label="Hai la patente?", options=("Si","No"))
+    
+
+
+    def patente(age,patente):
+        if age  >= 18 and patente == "Si":
+            st.write("Sei idoneo alla guida")
+        else:
+            st.write("Non sei idoneo alla guida")
+
+
+
+
+
+if __name__ == "__main__":
+    main()
+    
