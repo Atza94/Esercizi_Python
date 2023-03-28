79d6d24ed76ee517e6176818951de83ee9dcd697
Atza94 <andrea.atza@gmail.com>
2023-03-28 18:12:22 +0200

    funzionefuorimain

--- Es1/es1.py
+++ Es1/es1.py
@@ -1,6 +1,23 @@
 import streamlit as st
+ 
+ 
+st.title("Primo esercizio")
+st.header("Idoneità alla guida")
 
-def main():
+age=st.number_input(label="Inserisci la tua età", max_value=100, min_value=0 )
+
+patente=st.radio(label="Hai la patente?", options=("Si","No"))
+    
+
+
+def license (age,patente):
+    if age  >= 18 and patente == "Si":
+        st.write("Sei idoneo alla guida")
+    else:
+        st.write("Non sei idoneo alla guida")
+
+
+#def main():
     st.title("Primo esercizio")
     st.header("Idoneità alla guida")
 
@@ -20,6 +37,6 @@ def main():
 
 
 
-if __name__ == "__main__":
-    main()
+#if __name__ == "__main__":
+  # main()
     
