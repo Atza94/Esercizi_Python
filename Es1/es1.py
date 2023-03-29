d2b0f9df51bbc37754e3596db55cd398e117363d
Atza94 <andrea.atza@gmail.com>
2023-03-29 22:13:53 +0200

    more def

--- Es1/es1.py
+++ Es1/es1.py
@@ -4,47 +4,27 @@ import streamlit as st
 st.title("Primo esercizio")
 st.header("Idoneità alla guida")
 
-x=st.number_input(label="Inserisci la tua età", max_value=100, min_value=0, step=1)
+x = st.number_input(label="Inserisci la tua età", max_value=100, min_value=0, step=1)
 
-y = st.radio(label="Hai la patente?", options=("Si","No"), index=0)
+y = st.radio(label="Hai la patente?", options=("Si","No"))
 
-def idoneità(x,y):
-    x=st.number_input(label="Inserisci la tua età", max_value=100, min_value=0, step=1)
-    y = st.radio(label="Hai la patente?", options=("Si","No"), index=0)
-    if x >= 18 and y == [0]:
+def idoneita_eta(x):
+    if x >= "18":
         return True
     else:
         return False
 
-if idoneità == True:
-     st.write("abilitato alla guida")
+if idoneita_eta == True:
+     st.write("abilitato")
 else:
      st.write("non abilitato")
-
-st.button('conferma dati', on_click="idoneità()")
-
-
+   
         
 
    
 
 
 # #def main():
-#     st.title("Primo esercizio")
-#     st.header("Idoneità alla guida")
-
-#     age=st.number_input(label="Inserisci la tua età", max_value=100, min_value=0 )
-
-#     patente=st.radio(label="Hai la patente?", options=("Si","No"))
-    
-
-
-#     def license (age,patente):
-#         if age  >= 18 and patente == "Si":
-#             st.write("Sei idoneo alla guida")
-#         else:
-#             st.write("Non sei idoneo alla guida")
-
 
 
 
